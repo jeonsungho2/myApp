0f63018aeb98c1baf1f490a8ae501feca40c8b76
jeonsungho2 <jshdream4@naver.com>
2017-03-05 12:06:57 +0900

    커밋

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
 
 class App extends React.Component {
+
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        name: ''
+      };
+
+      this.setName = this.setName.bind(this);
+
+    }
+
+    setName() {
+      this.setState({
+        name: '전성호'
+      });
+    }
+
     render(){
 
         return (
-                <h1>Hello React Skeleton</h1>
+            <div>
+              <button onClick={this.setName} >click</button>
+              <h1>My Name is {this.state.name}ssss</h1>
+            </div>
         );
     }
 }
