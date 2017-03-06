6d637e8771902f997e052761f570784d04c22472
전성호 <jeonseongho@jeonseonghoui-MacBook-Pro.local>
2017-03-06 22:26:34 +0900

    2017/03/06/

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,11 @@
 import React from 'react';
+import Contact from './Contact';
 
 class App extends React.Component {
-
-    constructor(props) {
-      super(props);
-
-      this.state = {
-        name: ''
-      };
-
-      this.setName = this.setName.bind(this);
-
-    }
-
-    setName() {
-      this.setState({
-        name: '전성호'
-      });
-    }
-
-    render(){
-
-        return (
-            <div>
-              <button onClick={this.setName} >click</button>
-              <h1>My Name is {this.state.name}ssss</h1>
-            </div>
-        );
+    render() {
+      return (
+        <Contact/>
+      );
     }
 }
 
---
+++ src/components/Contact.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import ContactInfo from './ContactInfo';
+
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      keyword: '',
+      contactData: [{
+        name: 'Abet',
+        phone: '010-0000-0001'
+      }, {
+        name: 'Betty',
+        phone: '010-0000-0002'
+      }, {
+        name: 'Chalie',
+        phone: '010-0000-0003'
+      }, {
+        name: 'Candy',
+        phone: '010-0000-0004'
+      }]
+    };
+
+    this.hanleChange = this.hanleChange.bind(this);
+  }
+
+  hanleChange(e) {
+    this.setState({
+      keyword: e.target.value
+    });
+  }
+
+  render() {
+    const mapToComponets = (data) => {
+      data.sort();
+      data = data.filter(
+        (contact) => {
+          return contact.name.toLowerCase()
+              .indexOf(this.state.keyword.toLowerCase()) > -1;
+        }
+      );
+      return data.map((contact, i) => {
+        return (
+          <ContactInfo contact = {contact} key = {i} />
+        );
+      });
+    };
+
+    return (
+      <div>
+        <h1>Contacts</h1>
+        <input
+          name="keyword"
+          placeholder="Search"
+          value={this.state.keyword}
+          onChange={this.hanleChange}/>
+        <div>{mapToComponets(this.state.contactData)}</div>
+      </div>
+    )
+  }
+}
+
+export default Contact;
